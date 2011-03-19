1c3a0fc94cfcee084f8a0af9d51da98aa626c82a
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2011-03-19 16:42:39 +0100

    created simple rail3 app

--- exceptioner-mail/test/acceptance_test.rb
+++ exceptioner-mail/test/acceptance_test.rb
@@ -1,6 +1,8 @@
 require 'test/unit'
 class AcceptanceTest < Test::Unit::TestCase
   def test_rails_3
+
+
     assert_equal false, true
   end
 end
---
+++ exceptioner-mail/test/rails_3_0_3_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails303App::Application.config.secret_token = 'bd03674d5ea4c6cf9274b5404c1331f0bd54e419b314be89f95b6efcdc8581a03832ab859679260cd2ef9538e8ea4a656e135a2c64bf5945d163ba3c0b753008'
