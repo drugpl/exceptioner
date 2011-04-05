0e56db60281f28df83c578d6890bd93c8a6adc9b
Jan Dudek <jd@jandudek.com>
2011-04-05 17:41:09 +0200

    Rewrote tests for templates

--- exceptioner-core/test/template_test.rb
+++ exceptioner-core/test/template_test.rb
@@ -9,47 +9,47 @@ class TemplateTest < ExceptionerTestCase
     @env = { 'ruby' => '1.9.2', 'shell' => 'bash' }
     @controller = stub('ActionController::Base', :params => @controller_params, :controller_name => 'base', :action_name => 'test_action')
     @issue = Exceptioner::Issue.new(:controller => @controller, :exception => @exception, :env => @env)
-    @transport = Exceptioner::Transport::Testing.new(config)
+    @formatter = Exceptioner::Formatter.new([:backtrace, :env, :exception, :message, :params])
   end
 
-  def test_renders
-    assert_nothing_raised { @transport.render(@issue) }
+  def test_renders_everything
+    assert_nothing_raised { @formatter.render(@issue) }
   end
 
-  def test_render_controller_name
-    assert_match @issue.controller.controller_name, @transport.render(@issue)
+  def test_renders_template_backtrace
+    body = @formatter.render_partial(:backtrace, @issue)
+    assert_match @issue.formatted_backtrace, body
   end
 
-  def test_render_controller_action_name
-    assert_match @issue.controller.action_name, @transport.render(@issue)
-  end
+  def test_renders_template_env
+    body = @formatter.render_partial(:env, @issue)
 
-  def test_render_exception_message
-   assert_match @issue.exception.message, @transport.render(@issue)
-  end
-
-  def test_render_exception_class
-    assert_match @issue.exception.class.name, @transport.render(@issue)
-  end
-
-  def test_render_backtrace
-    assert_match @issue.formatted_backtrace, @transport.render(@issue)
-  end
-
-  def test_render_params
-    render = @transport.render(@issue)
-    @issue.params.each_pair do |k,v|
-      assert_match k.to_s, render
-      assert_match v.to_s, render
-    end
-  end
-
-  def test_render_params
-    render = @transport.render(@issue)
     @issue.env.each_pair do |k,v|
-      assert_match k.to_s, render
-      assert_match v.to_s, render
+      assert_match k.to_s, body
+      assert_match v.to_s, body
     end
   end
 
+  def test_renders_template_exception
+    body = @formatter.render_partial(:exception, @issue)
+
+    assert_match @issue.controller.controller_name, body
+    assert_match @issue.controller.action_name, body
+    assert_match @issue.exception.message, body
+    assert_match @issue.exception.class.name, body
+  end
+
+  def test_renders_template_message
+    body = @formatter.render_partial(:message, @issue)
+    assert_match @issue.message, body
+  end
+
+  def test_renders_template_params
+    body = @formatter.render_partial(:params, @issue)
+
+    @issue.params.each_pair do |k,v|
+      assert_match k.to_s, body
+      assert_match v.to_s, body
+    end
+  end
 end
